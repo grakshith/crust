39d6775b6af788081b63f6edc9249ae183c38837
Rakshith <hehaichi@gmail.com>
2020-05-29 14:25:29 -0700

    Add zero cost iterators program

---
+++ src/cxx/zero_cost_iterators.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+int main(){
+    std::vector<int> a,b;
+
+    for(int i=1; i<=50000; i++){
+        a.push_back(i);
+        b.push_back(10000-i);
+    }
+
+    std::vector<int> numbers;
+    numbers.resize(50000);
+
+    std::transform(a.begin(), a.end(), a.begin(), [](int num){ return std::pow(num, 2);});
+    std::copy_if(a.begin(), a.end(), numbers.begin(), [](int num){return num>50000;});
+
+    std::vector<int> num;
+    num.resize(4);
+    std::copy_n(numbers.begin(), 4, num.begin());
+
+    for(int i=0; i<4; i++){
+        std::cout<<num[i]<<std::endl;
+    }
+
+}
