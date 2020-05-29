b8849a447a1169c41edf8aa86b5794fac322baed
Rakshith <hehaichi@gmail.com>
2020-05-28 23:21:21 -0700

    Add dynamic dispatch program

---
+++ src/cxx/dyn_dispatch.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+
+class Sample{
+    public:
+    void static_dispatch(int a){
+        std::cout<<"A is "<<a<<std::endl;
+    }
+
+    virtual void dyn_dispatch(int a){
+        std::cout<<"The value of A is "<<a<<std::endl;
+    }
+};
+
+class InheritSample: public Sample{
+    public:
+    void static_dispatch(int b){
+        std::cout<<"B is "<<b<<std::endl;
+    }
+    virtual void dyn_dispatch(int b){
+        std::cout<<"B's value is "<<b<<std::endl;
+    }
+};
+
+int main(){
+    Sample *s = new InheritSample();
+
+    Sample ss;
+
+    s->static_dispatch(5);
+    s->dyn_dispatch(5);
+
+    ss.dyn_dispatch(50);
+
+    InheritSample is;
+    is.dyn_dispatch(40);
+
+    ss=(Sample)is;
+    ss.dyn_dispatch(30);
+}
