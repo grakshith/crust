4bd31da97b1770d3dff3f34ebc4e73153668863e
Rakshith <hehaichi@gmail.com>
2020-05-30 11:50:53 -0700

    Update move program

--- src/cxx/move.cc
+++ src/cxx/move.cc
@@ -4,10 +4,14 @@
 
 int main(){
     std::string a = "Hello world";
+    std::cout<<&a<<std::endl;
     std::vector<std::string> vec;
 
     vec.push_back(std::move(a));
 
     std::cout<<vec[0]<<std::endl;
+    std::cout<<&vec[0]<<std::endl;
     std::cout<<a<<std::endl;
+    a="New hello world";
+    std::cout<<&a<<std::endl;
 }
