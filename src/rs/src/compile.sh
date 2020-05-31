25624343939ee7b6e37860cc578fadeb1bcc2dcb
Rakshith <hehaichi@gmail.com>
2020-05-31 16:14:44 -0700

    Update makefile to use O3 optimization

--- src/rs/src/compile.sh
+++ src/rs/src/compile.sh
@@ -1,1 +1,1 @@
-find . -type f \( -iname "*.rs" ! -iname "*.sh" \) -print -exec rustc --out-dir ../bin {} \;
+find . -type f \( -iname "*.rs" ! -iname "*.sh" \) -print -exec rustc -C opt-level=3 --out-dir ../bin {} \;
