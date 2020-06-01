578306881e96a80ea92dfb5de32be933a5309a65
Rakshith <hehaichi@gmail.com>
2020-05-31 18:28:01 -0700

    Add vector copy and move programs

---
+++ src/cxx/vector_copy.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    std::vector<int> vec;
+    for(int i=0; i<10000000; i++){
+        vec.push_back(i);
+    }
+
+    std::cout<<vec[vec.size()-1]<<"\n";
+    std::vector<int> vec_2(vec);
+
+    std::cout<<vec[vec.size()-1]<<"\n";
+    std::cout<<vec_2[vec_2.size()-1]<<"\n";
+
+}
