123239a78b8bfa344cde1121609a185558f51f74
Rakshith <hehaichi@gmail.com>
2020-05-31 23:24:38 -0700

    Update mem leak code

--- src/cxx/shared_ptr_mem_leak.cc
+++ src/cxx/shared_ptr_mem_leak.cc
@@ -12,19 +12,25 @@ public:
 };
 
 int main(){
-    Node a(1);
-    Node b(2);
-    Node c(3);
-    Node d(4);
+    Node *a = new Node(1);
+    Node *b = new Node(2);
+    Node *c = new Node(3);
+    Node *d = new Node(4);
 
-    a.next = std::make_shared<Node>(b);
-    b.prev = std::make_shared<Node>(a);
+    std::shared_ptr<Node> a_ptr, b_ptr, c_ptr, d_ptr;
+    a_ptr = std::make_shared<Node>(*a);
+    b_ptr = std::make_shared<Node>(*b);
+    c_ptr = std::make_shared<Node>(*c);
+    d_ptr = std::make_shared<Node>(*d);
 
-    b.next = std::make_shared<Node>(c);
-    c.prev = std::make_shared<Node>(d);
+    a->next = b_ptr;
+    b->prev = a_ptr;
 
-    c.next = std::make_shared<Node>(d);
-    d.prev = std::make_shared<Node>(c);
+    b->next = c_ptr;
+    c->prev = b_ptr;
+
+    c->next = d_ptr;
+    d->prev = c_ptr;
 
 
 }
