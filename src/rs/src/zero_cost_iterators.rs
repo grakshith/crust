66b05143f47dc91da96cb125015a23d39b197df2
Subramaniyam Shankar (foofooman) <subramaniyam.shankar1996@gmail.com>
2020-06-08 13:44:55 -0700

    Fix comment

--- src/rs/src/zero_cost_iterators.rs
+++ src/rs/src/zero_cost_iterators.rs
@@ -14,11 +14,11 @@ fn main() {
 	}
 
 	let numbers = a.iter()	// creating an iterator from a
-						.zip(b.iter())	// making a pair of (a, b)
-						.map(|(a, b)| a * b) // taking product of each element
-						.filter(|a| *a > 5000) // select only those elements that are greater than 20
-						.take(4) // select at most 4 elements
-						.collect::<Vec<_>>(); // the place where iteration finally happens
+			.zip(b.iter())	// making a pair of (a, b)
+			.map(|(a, b)| a * b) // taking product of each element
+			.filter(|a| *a > 5000) // select only those elements that are greater than 5000
+			.take(4) // select at most 4 elements
+			.collect::<Vec<_>>(); // the place where iteration finally happens
 											// and collect it as a vector
 
 	println!("{:?}", numbers);
